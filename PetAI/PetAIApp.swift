86f3fd4429c29940ef2cfdeef8e2589f60f3089b
Carlos Mbendera <carlosmbendera@gmail.com>
2025-04-04 23:31:49 -0500

    Initial Commit

---
+++ PetAI/PetAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PetAIApp.swift
+//  PetAI
+//
+//  Created by Carlos Mbendera on 04/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct PetAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
